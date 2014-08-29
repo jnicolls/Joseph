12781924b229f38f74da836a23b888d7c012c747
jnicolls <jnicolls@stanford.edu>
2014-08-29 11:43:40 -0700

    Fixed bug in .js

--- webpage.js
+++ webpage.js
@@ -1,6 +1,3 @@
 var length;
 length = 6;
-document.getElementById('test').innerHTML = length
-var date;
-date = {DATE_ATOM};
-document.getElementById('date').innnerHTML = date
+document.getElementById('test').innerHTML = length;

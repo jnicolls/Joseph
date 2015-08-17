7c22fa5e3e3ff886426d153a29d355ecdf93b9c2
root <jnicolls@stanford.edu>
2015-08-17 16:04:23 -0700

    Added comments to reflect work done during sprint.

--- absXpath.py
+++ absXpath.py
@@ -1,9 +1,19 @@
+# absXpath.py takes in three arguments from the command line. The first two represent filepaths to XML files. 
+# The last represents a tag that is being searched for. 
+#
+# This program goes trhough every element in the second argument (the 'convertFile'), and tries to find a matching
+# element in the first, both in terms of the tag content and in terms of the string representation of the xpath. 
+
 import sys
 import lxml
 from lxml import etree
 
+# The tag will remain as an unaltered string. The sampleFile and convertFile will 
+# be parsed according to the ElementTree structure in python. 
+
 sampleFile = open(sys.argv[1], 'r')
 convertFile = 	open(sys.argv[2], 'r') 
+tag = sys.argv[3]
 
 convertTree = lxml.etree.parse(convertFile)
 sampleTree = lxml.etree.parse(sampleFile)
@@ -11,16 +21,16 @@ sampleTree = lxml.etree.parse(sampleFile)
 convertFile.close()
 sampleFile.close()
 
+# This variable keeps track of whether all of the current tags in the converted XML has 
+# both a matching xpath and a matching body text. 
 
 matchSoFar = True
 
-tag = sys.argv[3]
+
+
 for convertElem in convertTree.findall(tag):
 	xp = convertTree.getpath(convertElem)
 	sampleElemList = sampleTree.findall(xp)
-	print xp
-	print convertElem.text
-	print sampleElemList
 	if sampleElemList == None:
 		matchSoFar = False
 		break
@@ -31,6 +41,9 @@ for convertElem in convertTree.findall(tag):
 			break
 	if matchSoFar == False:
 		break
+
+# pathTestResults is a constant file that a corresponding RobotFramework test will 
+# check the contents of to observe the results of the test. 
 
 result = open('pathTestResults', 'w')
 

ae34689fe58e7234314b0d457d7961f75f7fef70
Joseph Nicolls <jnicolls@stanford.edu>
2016-01-12 15:19:05 -0800

    Update testvars.py

--- testvars.py
+++ testvars.py
@@ -5,10 +5,11 @@
 # In the event that the test is transferred to a different system, variables in this file must be adjusted depending on file organization. 
 #
 
-convertParentPath = '/var/local/meTypesetTests/tests/testOutput'
-sampleParentPath = '/var/local/meTypesetTests/testingDocuments/OpenMedXML'
-AcceptableErrorRegex = '(OpenMed-\\d*-e*\\d*?..docx/nlm/out.xml:\\d*: element xref: validity error : IDREFS attribute rid references an unknown ID "TO_LINK")?(Document OpenMed-\\d*-e*\\d*?..docx/nlm/out.xml does not validate against http://jats.nlm.nih.gov/archiving/1.1d3/JATS-archivearticle1.dtd)'
-demoParentPath = '/var/local/meTypesetTests/tests/newCorpus'
-errorMessage = 'does not match up evenly between the sample XML and the meTypeset\'d version. Please see corresponding file for more information!'
-necessaryMatchElems = ['./article/title', './/aff', './/p', './/abstract/p', './/fig/graphic', './/fig/table-wrap/table', './/sec/title', ' .//fig/table-wrap/caption', ' .//fn/fn-group']
-~                                                                                                                                                                                               
+frontConvertParent = '/var/local/meTypesetTests/testingDocuments/jolOutput'
+frontSampleParent = '/var/local/meTypesetTests/testingDocuments/jolXML'
+bodyConvertParent = '/var/local/meTypesetTests/testingDocuments/coactionDocxOutput/'
+bodySampleParent = '/var/local/meTypesetTests/testingDocuments/coactionXML'
+necessaryMatchElems = ['body//sec/title', 'body//table-wrap//caption', 'body//list/list-item/p', 'front//email', 'front//journal-title', 'front//article-title', 'front//aff', 'back//fn', 'front//abstract//p' , 'body//fig//caption', 'front//name']
+levThreshold = 30
+acceptableFailureProportion = .25
+acceptableFailurePercentage = '25%'
